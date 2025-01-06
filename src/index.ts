9d290e7299fe2e9568b912aeba0a261fd133ad6c
unknown <vaibhavpratham507@gmail.com>
2025-01-06 18:44:31 +0530

    CORS issue

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,13 @@ import { middleware } from "./middleware";
 import { z } from "zod";
 import bcrypt from "bcrypt";
 import { random } from "./util";
+import cors from "cors";
 
 
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
 app.post("/api/v1/signup", async (req, res) => {
 
